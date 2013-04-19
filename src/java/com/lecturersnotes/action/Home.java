f6bf00f7bbef0f345b80da275a74461bc03e3373
bashashaik <basha3232@gmail.com>
2013-04-19 09:57:37 +0530

    2

---
+++ src/java/com/lecturersnotes/action/Home.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lecturersnotes.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author Basha Shaik
+ */
+public class Home extends ActionSupport {
+    
+    public Home() {
+    }
+    
+    public String execute() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public String upload()throws Exception {
+        return SUCCESS;
+    }
+}
--- web/home.jsp
+++ web/home.jsp
@@ -5,6 +5,7 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="s" uri="/struts-tags" %>
 <!DOCTYPE html>
 <html>
     <head>
@@ -12,6 +13,12 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+    <center>
+        <h3>Welcome</h3>
+        <s:form action="upload" enctype="multipart/form-data">
+            <s:file key="file" label="Select your file " />
+            <s:submit value="upload"/>
+        </s:form>
+    </center>
     </body>
 </html>
